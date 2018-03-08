999728684c7a38a7470bce7f379199ef12123ac1
Unknown <kevin63857@gmail.com>
2018-03-08 15:10:25 -0500

    Testing with new method of building up the board

--- color_tower.py
+++ color_tower.py
@@ -5,6 +5,7 @@ import array
 import time
 import sys
 import itertools
+import random
 #Provide the list of things to pick from, and how many to pick.
 #Returns a list of all permutations
 def nPr(data,r):
@@ -27,6 +28,20 @@ def printBoardConfig(s1,s2,s3,s4,s5,s6):
             [2,3,6,1,5,4],
             [1,5,4,2,3,6]]
     perms=[None,s1,s2,s3,s4,s5,s6]
+    for i in perms:
+        if i is None:
+            continue
+        for i2 in i:
+            sys.stdout.write(nToL[i2])
+            sys.stdout.write(' ')
+        print ''
+
+    print ''
+    print ''
+    for i in board:
+        print i
+    print ''
+    print ''
     cols=[[0 for i in range(0,6)]for i2 in range(0,6)]
     cur_row=[0]*6
     for row_num,row in enumerate(board):
@@ -40,6 +55,63 @@ def printBoardConfig(s1,s2,s3,s4,s5,s6):
         sys.stdout.write(str(sum(i)))
         sys.stdout.write(' ')
     print ' '
+
+def printBoardConfig_fourArg(s1,s2,s3,s4):
+    print ''
+    nToL={1:'R',2:'G',4:'B',8:'Y',16:'P',32:'O'}
+    board= [[3,2,1,6,4,5],
+            [6,4,2,5,1,3],
+            [4,1,5,3,6,2],
+            [5,6,3,4,2,1],
+            [2,3,6,1,5,4],
+            [1,5,4,2,3,6]]
+    perms=[None,s1,s2,s3,s4]
+    cols=["" for i2 in range(0,6)]
+    for row_num,row in enumerate(board):
+        cur_row=""
+        for col_num,val in enumerate(row):
+            if val>4:
+                sys.stdout.write('  ')
+                continue
+            sys.stdout.write(nToL[perms[val][row_num]])
+            sys.stdout.write(' ')
+            str_val=str(perms[val][row_num])
+            if (str_val in cur_row) or (str_val in cols[col_num]):
+                print "FAILED"
+                return False
+            cur_row+=str_val
+            cols[col_num]+=str_val
+        print ''
+    print cols
+    return True
+
+def printBoardConfig_nArg(perms):
+    #print ''
+    nToL={1:'R',2:'G',4:'B',8:'Y',16:'P',32:'O'}
+    board= [[3,2,1,6,4,5],
+            [6,4,2,5,1,3],
+            [4,1,5,3,6,2],
+            [5,6,3,4,2,1],
+            [2,3,6,1,5,4],
+            [1,5,4,2,3,6]]
+    cols=["" for i2 in range(0,6)]
+    for row_num,row in enumerate(board):
+        cur_row=""
+        for col_num,val in enumerate(row):
+            if val>=len(perms):
+                #sys.stdout.write('  ')
+                continue
+            #sys.stdout.write(nToL[perms[val][row_num]])
+            #sys.stdout.write(' ')
+            str_val=str(perms[val][row_num])
+            if (str_val in cur_row) or (str_val in cols[col_num]):
+                #print "FAILED"
+                return False
+            cur_row+=str_val
+            cols[col_num]+=str_val
+        #print ''
+    #print cols
+    return True
     #print nToL[s3[0]],nToL[s2[0]],nToL[s1[0]],nToL[s6[0]],nToL[s4[0]],nToL[s5[0]]
 
 def runOpenCL():
@@ -87,10 +159,10 @@ def runOpenCL():
         for(int row=0;row<6;row++){
             int vals[6]={0,0,0,0,0,0};
             for(int col=0;col<6;col++){
-                if(board[row][col]>3){
+                if(board[row][col]>4){
                     continue;
                 }
-                if(++vals[color_permutations[6*s[board[row][col]]+row]]>1){
+                if(++vals[color_permutations[6*s[board[row][col]-1]+row]]>1){
                     good_set=false;
                 }
             }
@@ -102,14 +174,14 @@ def runOpenCL():
                 if(board[row][col]>3){
                     continue;
                 }
-                if(++vals[color_permutations[6*s[board[row][col]]+row]]>1){
+                if(++vals[color_permutations[6*s[board[row][col]-1]+row]]>1){
                     good_set=false;
                 }
             }
         }
         if(!good_set){
-            s5s[gid]=-1;
-            s6s[gid]=-1;
+            s5s[gid]=-2;
+            s6s[gid]=-2;
             return;
         }
         bool found=false;
@@ -150,8 +222,17 @@ def runOpenCL():
     print time.time()-start
     cl.enqueue_copy(queue,s5s,s5s_buffer)
     cl.enqueue_copy(queue,s6s,s6s_buffer)
+    minus1=0
+    minus2=0
     for idx,i in enumerate(s5s):
-        if i!=-1:
+        if idx%100000==0 and idx>0:
+            print (float(idx)/len(s5s))*100
+            print minus1,minus2
+        if i==-1:
+            minus1+=1
+        elif i==-2:
+            minus2+=1
+        else:
             print idx,i,s6s[idx]
             decode=idx
             s2=decode%720;
@@ -165,5 +246,42 @@ if __name__ == '__main__':
     colorPerms=nPr((1,2,4,8,16,32),6)
     #print len(itertools.combinations_with_replacement([1,2,4,8,16,32],6))
     printBoardConfig(colorPerms[0],colorPerms[10],colorPerms[200],colorPerms[300],colorPerms[500],colorPerms[600])
+
+    printBoardConfig_fourArg(colorPerms[24],colorPerms[406],colorPerms[193],colorPerms[612])
     #exit()
+    while True:
+        for val0 in range(0,720):
+            if not printBoardConfig_nArg([None,colorPerms[val0]]):
+                continue
+            #print val0
+            for val1 in range(0,720):
+                if not printBoardConfig_nArg([None,colorPerms[val0],colorPerms[val1]]):
+                    continue
+                #print val0,val1
+                for val2 in range(0,720):
+                    if not printBoardConfig_nArg([None,colorPerms[val0],colorPerms[val1],colorPerms[val2]]):
+                        continue
+                    #print val0,val1,val2
+                    for val3 in range(0,720):
+                        if not printBoardConfig_nArg([None,colorPerms[val0],colorPerms[val1],colorPerms[val2],colorPerms[val3]]):
+                            continue
+                        print val0,val1,val2,val3
+                        for val4 in range(0,720):
+                            if not printBoardConfig_nArg([None,colorPerms[val0],colorPerms[val1],colorPerms[val2],colorPerms[val3],colorPerms[val4]]):
+                                continue
+                            print val0,val1,val2,val3,val4
+                            exit()
+                            for val5 in range(0,720):
+                                if printBoardConfig_nArg([None,colorPerms[val0],colorPerms[val1],colorPerms[val2],colorPerms[val3],colorPerms[val4],colorPerms[val5]]):
+                                    exit()
+                                    print val0,val1,val2,val3,val4
+        exit()
+    while True:
+        val1=random.randint(0,719)
+        val2=random.randint(0,719)
+        val3=random.randint(0,719)
+        if printBoardConfig_fourArg(colorPerms[0],colorPerms[val1],colorPerms[val2],colorPerms[val3]):
+            print val1,val2,val3
+            exit()
+    exit()
     runOpenCL()
